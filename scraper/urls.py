d0ffaa5d17c8cfcd64bd6a00456fbd3ecee5dcc6
Cristian Blanco <cristiamblancogarcia@gmail.com>
2025-04-30 10:33:34 -0600

    Add urls for login/logout functionality

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'scraper'
 
 urlpatterns = [
     path('', views.app_list, name='app_list'),
-] 
+    path('login/', auth_views.LoginView.as_view(
+        template_name='scraper/login.html',
+        redirect_authenticated_user=True
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='scraper:login',
+        template_name='scraper/login.html'
+    ), name='logout'),
+]
