061895e7f90a168ff4856bce65e27a2e96f19ea2
Cristian Blanco <cristiamblancogarcia@gmail.com>
2025-04-29 22:33:50 -0600

    migration to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'base',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # Or your database host
+        'PORT': '5432',      # Default PostgreSQL port
     }
 }
 
